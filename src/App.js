62a419e91fcb73a9447c03081a8a2b02bacf02c3
lrq <lrq@lrq.lr>
2020-04-18 11:18:51 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = (props) => {
+    return (
+        <div className="main">
+          <h1>Hello worl</h1>
+        </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+module.exports = (env, argv) =>  {
+  const dev = argv.mode === 'development';
+  return {
+
+    // Map source files for better errors
+    devtool: 'source-map',
+    entry: {
+      bundle: './src/index.js'
+    },
+    output: {
+      path: path.join(__dirname, dev ? './devBuild' : './build'),
+      filename: '[name].js',
+      publicPath: '/'
+    },
+    devServer: {
+      contentBase: path.join(__dirname, './src'),
+      watchContentBase: true,
+      compress: true,
+      port: 3000,
+
+      // Show client errors in browser
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: [
+            /node_modules/,
+          ],
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              '@babel/preset-react',
+              [
+                '@babel/preset-env',
+                {
+                  'include': [
+                    '@babel/plugin-proposal-object-rest-spread'
+                  ],
+                  'targets': {
+                    'node': '10'
+                  }
+                }
+              ],
+            ],
+          },
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            require.resolve('style-loader'),
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            },
+          ]
+        }
+      ],
+    },
+  };
+};
